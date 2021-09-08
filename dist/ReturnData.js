d430a747750f31f7a3c290efbf9d1cb5f209ab60
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-09-08 17:04:33 +0800

    feat: Init Project

---
+++ dist/ReturnData.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function index() {
+    return "return-data";
+}
+exports.default = index;
+//# sourceMappingURL=ReturnData.js.map
---
+++ dist/return-data.es.js
@@ -0,0 +1,6 @@
+function index() {
+    return "return-data";
+}
+
+export { index as default };
+//# sourceMappingURL=return-data.es.js.map
